4a6dc44afa885242788e4fe4251277b083c0d790
Joshua-2333 <jaflores.2333@gmail.com>
2026-01-11 00:09:07 -0600

    fixed 500 error on login page when using session store by adding sid to Session model

---
+++ prisma/migrations/20260111060748_add_sid_to_session/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[sid]` on the table `Session` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `sid` to the `Session` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Session" DROP CONSTRAINT "Session_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Session" ADD COLUMN     "sid" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
+
+-- CreateIndex
+CREATE INDEX "Session_sid_idx" ON "Session"("sid");
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
